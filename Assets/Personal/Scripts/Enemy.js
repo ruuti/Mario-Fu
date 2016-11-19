961385d130a7ba5eec9e8cb32b6305f359114d27
Misael Berrios Salas <Misael Berrios Salas>
2016-11-19 02:14:46 +0100

    almost!

--- Assets/Personal/Scripts/Enemy.js
+++ Assets/Personal/Scripts/Enemy.js
@@ -21,7 +21,14 @@ function FixedUpdate ()
     var konstant = moveLeft ? -1: 1;
     enemy.position.x = enemy.position.x + konstant * 0.1;
 
-    if (Mathf.Round(enemy.position.x) == Mathf.Round(player.position.x) && Mathf.Round(player.position.y) < 2.561593 && !hit){
+    var distanceX = enemy.position.x - player.position.x;
+    var distanceY = enemy.position.y - player.position.x;
+    
+    var isCloseX = distanceX < 0.5 && distanceX > -0.5;
+    var isCloseY = distanceY < 0.5 && distanceY > -0.5;
+
+    //if (Mathf.Round(enemy.position.x) == Mathf.Round(player.position.x) && Mathf.Round(player.position.y) < 11 && !hit){
+    if (isCloseX && isCloseY && !hit){
     	hit = true;
     	var i=0;
     	var m = 0.1F;
@@ -33,12 +40,12 @@ function FixedUpdate ()
 		if(i == 10) {
 			hit = false;
 			player.position.x = -5.146368;
-			player.position.y = 2.561593;
+			player.position.y = 20;
 
 			player.localScale = new Vector3(1.771533, 1.771533, 0);
 
 			enemy.position.x = 10.146368;
-			enemy.position.y = 1.180461;
+			enemy.position.y = 20;
 
 		}
     }
